ea00f22ffccc5beee550668f8e151f9b34ecfa4d
Pedro Paulo <silva.pedropaulosantos@gmail.com>
2021-02-05 17:48:28 -0300

    Expondo o header Authorizathion

--- src/main/java/com/curso/spring/security/JWTAuthenticationFilter.java
+++ src/main/java/com/curso/spring/security/JWTAuthenticationFilter.java
@@ -61,6 +61,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String username = ((UserSS) authResult.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
 		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("acces-control-expose-headers", "Authorization");
 	}
 	
 	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
